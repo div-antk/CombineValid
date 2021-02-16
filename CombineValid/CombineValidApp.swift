44274e8bde4e29e4a6c374eca7dfe5779a63d0d1
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-16 21:50:29 +0900

    Initial Commit

---
+++ CombineValid/CombineValidApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineValidApp.swift
+//  CombineValid
+//
+//  Created by Takuya Ando on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct CombineValidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineValid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineValid
+//
+//  Created by Takuya Ando on 2021/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
